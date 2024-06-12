d105d7d474e3451db78bcebee69d2164c78428a5
zhu-chen <1040555811@qq.com>
2024-06-12 17:56:09 +0800

    feat:finish C version of BBT

--- T2/BBT.c
+++ T2/BBT.c
@@ -1,221 +1,182 @@
 #include <stdio.h>
-#define ls tree[x].l
-#define rs tree[x].r
-using namespace std;
-const int MAXN=1e5+10;
-int cnt,T,root;
-inline int read()
-{
-	int s=0,f=1;char ch=getchar();
-	while(ch<'0'||ch>'9'){
-		if(ch=='-')f=-1;
-		ch=getchar();
-	}
-	while(ch>='0'&&ch<='9'){
-		s=s*10+ch-'0';
-		ch=getchar();
-	}
-	return s*f;
-}
-struct treap{
-	int val,data,l,r,size;
-}tree[MAXN];
+#include <stdlib.h>
+#include <string.h>
 
-void pushup(int x)
-{
-	tree[x].size=tree[ls].size+tree[rs].size+1;
-}
+#define MAXN 100010
 
-void rotate_r(int &x)
-{
-//	cout<<"r"<<x<<" "<<ls;
-	int ll=ls; 
-	tree[x].l=tree[ll].r;
-	tree[ll].r=x;
-	tree[ll].size=tree[x].size;
-	pushup(x);
-	x=ll; 
+struct treap {
+	int val, data, l, r, size;
+};
+
+int cnt, T, root;
+struct treap tree[MAXN];
+
+int read() {
+	int s = 0, f = 1;
+	char ch = getchar();
+	while (ch < '0' || ch > '9') {
+		if (ch == '-') f = -1;
+		ch = getchar();
+	}
+	while (ch >= '0' && ch <= '9') {
+		s = s * 10 + ch - '0';
+		ch = getchar();
+	}
+	return s * f;
 }
 
-void rotate_l(int &x)
-{
-//	cout<<"l"<<x<<" "<<rs;
-	int rr=rs;
-	tree[x].r=tree[rr].l;
-	tree[rr].l=x;
-	tree[rr].size=tree[x].size;
-	pushup(x);
-	x=rr;
+void pushup(int x) {
+	tree[x].size = tree[tree[x].l].size + tree[tree[x].r].size + 1;
 }
 
-/*long long random(int x){
-	return (long long)rand()*rand()%x+1;
-} */
-
-void insert(int &x,int goal)
-{
-	if(x==0){
-		
-		x=++cnt;
-//		cout<<"insert"<<goal<<" "<<x<<" "<<root<<endl;
-		tree[x].size=1;
-		tree[x].data=goal;
-		tree[x].val=(rand()*rand())%20040610;
-		return ;
-	}
-	tree[x].size++;
-	if(goal>=tree[x].data){
-		insert(rs,goal);
-	}else{
-		insert(ls,goal);
-	}
-	if(ls!=0&&tree[x].val>tree[ls].val){
-		
-		rotate_r(x);
-//		cout<<ls<<" "<<x<<"Rl"<<root<<endl;
-	}
-	if(rs!=0&&tree[x].val>tree[rs].val){
-		rotate_l(x);
-//		cout<<x<<"Ll"<<root<<endl;
-	}
-	pushup(x);
+void rotate_r(int *x) {
+	int ll = tree[*x].l;
+	tree[*x].l = tree[ll].r;
+	tree[ll].r = *x;
+	tree[ll].size = tree[*x].size;
+	pushup(*x);
+	*x = ll;
 }
 
-void del(int &x,int goal)
-{
-	/*if(x==0){
+void rotate_l(int *x) {
+	int rr = tree[*x].r;
+	tree[*x].r = tree[rr].l;
+	tree[rr].l = *x;
+	tree[rr].size = tree[*x].size;
+	pushup(*x);
+	*x = rr;
+}
+
+void insert(int *x, int goal) {
+	if (*x == 0) {
+		*x = ++cnt;
+		tree[*x].size = 1;
+		tree[*x].data = goal;
+		tree[*x].val = (rand() * rand()) % 20040610;
 		return;
-	}*/
-//	cout<<x<<" "<<tree[x].data<<" "<<ls<<" "<<rs<<" "<<goal<<"del\n";
-	tree[x].size--;
-	if(tree[x].data==goal){
-//		cout<<"ddddddddddddd\n";
-		if(ls==0&&rs==0){
-//			cout<<"-----"<<x<<endl;
-			x=0;
+	}
+	tree[*x].size++;
+	if (goal >= tree[*x].data) {
+		insert(&tree[*x].r, goal);
+	} else {
+		insert(&tree[*x].l, goal);
+	}
+	if (tree[*x].l != 0 && tree[*x].val > tree[tree[*x].l].val) {
+		rotate_r(x);
+	}
+	if (tree[*x].r != 0 && tree[*x].val > tree[tree[*x].r].val) {
+		rotate_l(x);
+	}
+	pushup(*x);
+}
+
+void del(int *x, int goal) {
+	tree[*x].size--;
+	if (tree[*x].data == goal) {
+		if (tree[*x].l == 0 && tree[*x].r == 0) {
+			*x = 0;
 			return;
-		}else if(ls==0||rs==0){
-			x=ls+rs;
-//			cout<<"++++"<<x<<endl;
+		} else if (tree[*x].l == 0 || tree[*x].r == 0) {
+			*x = tree[*x].l + tree[*x].r;
 			return;
 		}
-		if(tree[ls].val<tree[rs].val){
+		if (tree[tree[*x].l].val < tree[tree[*x].r].val) {
 			rotate_r(x);
-			del(rs,goal);
+			del(&tree[*x].r, goal);
 			return;
-		}else{
+		} else {
 			rotate_l(x);
-			del(ls,goal);
+			del(&tree[*x].l, goal);
 			return;
 		}
 	}
-	if(tree[x].data>=goal){
-//		cout<<"dell\n";
-		del(ls,goal);
-	}else{
-//		cout<<"delr\n";
-		del(rs,goal);
+	if (tree[*x].data >= goal) {
+		del(&tree[*x].l, goal);
+	} else {
+		del(&tree[*x].r, goal);
 	}
-	pushup(x);
+	pushup(*x);
 }
 
-int kth(int x,int goal)
-{
-	if(x==0){
+int kth(int x, int goal) {
+	if (x == 0) {
 		return 0;
 	}
-	if(goal<=tree[x].data){
-		return kth(ls,goal);
-	}else{
-		return tree[ls].size+1+kth(rs,goal);
+	if (goal <= tree[x].data) {
+		return kth(tree[x].l, goal);
+	} else {
+		return tree[tree[x].l].size + 1 + kth(tree[x].r, goal);
 	}
 }
 
-int query(int x,int k)
-{
-	if(k==tree[ls].size+1){
+int query(int x, int k) {
+	if (k == tree[tree[x].l].size + 1) {
 		return tree[x].data;
-	}else if(k<tree[ls].size+1){
-		return query(ls,k);
-	}else{
-		return query(rs,k-tree[ls].size-1);
+	} else if (k < tree[tree[x].l].size + 1) {
+		return query(tree[x].l, k);
+	} else {
+		return query(tree[x].r, k - tree[tree[x].l].size - 1);
 	}
 }
 
-int pre(int x,int goal)
-{
-	if(x==0){
+int pre(int x, int goal) {
+	if (x == 0) {
 		return 0;
 	}
-	if(tree[x].data>=goal){
-		return pre(ls,goal);
-	}else{
-		int rnum=pre(rs,goal);
-		if(rnum==0){
+	if (tree[x].data >= goal) {
+		return pre(tree[x].l, goal);
+	} else {
+		int rnum = pre(tree[x].r, goal);
+		if (rnum == 0) {
 			return tree[x].data;
-		}else{
+		} else {
 			return rnum;
 		}
 	}
 }
 
-int succ(int x,int goal)
-{
-//	cout<<x<<" "<<ls<<" "<<rs<<"succ\n";
-	if(x==0){
+int succ(int x, int goal) {
+	if (x == 0) {
 		return 0;
 	}
-	if(tree[x].data<=goal){
-//		cout<<"0\n";
-		return succ(rs,goal);
-	}else{
-		int lnum=succ(ls,goal);
-		if(lnum==0){
-//			cout<<"1\n";
+	if (tree[x].data <= goal) {
+		return succ(tree[x].r, goal);
+	} else {
+		int lnum = succ(tree[x].l, goal);
+		if (lnum == 0) {
 			return tree[x].data;
-		}else{
-//			cout<<"2\n";
+		} else {
 			return lnum;
 		}
 	}
 }
 
-signed main()
-{
-//	freopen(".in","r",stdin);
-//	freopen(".out","w",stdout);
-	memset(tree,0,sizeof(tree));
+int main() {
+	memset(tree, 0, sizeof(tree));
 	srand(71806291);
-	cin>>T;
-	for(int nt=1;nt<=T;++nt){
-		int opt=read(),xx=read();
-		switch(opt){
+	T = read();
+	for (int nt = 1; nt <= T; ++nt) {
+		int opt = read(), xx = read();
+		switch (opt) {
 			case 1:
-				insert(root,xx);
-//				cout<<root<<"root"<<endl;
+				insert(&root, xx);
 				break;
 			case 2:
-				del(root,xx);
-//				cout<<root<<"root"<<endl;
+				del(&root, xx);
 				break;
 			case 3:
-//				cout<<root<<"root"<<endl;
-				cout<<kth(root,xx)+1<<endl;
+				printf("%d\n", kth(root, xx) + 1);
 				break;
 			case 4:
-//				cout<<root<<"root"<<endl;
-				cout<<query(root,xx)<<endl;
+				printf("%d\n", query(root, xx));
 				break;
 			case 5:
-//				cout<<root<<"root"<<endl;
-				cout<<pre(root,xx)<<endl;
+				printf("%d\n", pre(root, xx));
 				break;
 			case 6:
-//				cout<<root<<"root"<<endl;
-				cout<<succ(root,xx)<<endl;
+				printf("%d\n", succ(root, xx));
 				break;
 		}
 	}
 	return 0;
 }
-//旋转treap 
