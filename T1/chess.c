5565e54caf6b62ba9bc647128ed7c2ca586031e2
56dd <zhuxuyuan22@mails.ucas.ac.cn>
2024-06-13 16:33:07 +0800

    修改了头文件

--- T1/chess.c
+++ T1/chess.c
@@ -4,6 +4,9 @@ int chessboard[8][8];
 
 int main(){
     //TODO: ADD YOUR CODE HERE
-
+    int x,y;
+    scanf("%d %d",&x,&y);
+    
+    print_chessboard();
     return 0;
 }
--- T1/chess.h
+++ T1/chess.h
@@ -1,7 +1,61 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 extern int chessboard[8][8];
 
 void print_chessboard();
+
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
+#define ERROR 0
+#define OK 1
+typedef char SElemType;
+typedef struct{
+    SElemType *base;
+    SElemType *top;
+    int stacksize;
+}Sqstack;
+int push(Sqstack *S ,SElemType e);
+int pop(Sqstack *S,SElemType *e);
+int InitStack(Sqstack *S);
+int GetTop(Sqstack *S,SElemType *e);
+int InitStack(Sqstack *S)
+{
+    S->base=(SElemType*)malloc(STACK_INIT_SIZE*sizeof(SElemType));
+    if(!S->base) 
+    return ERROR;
+    S->top=S->base;
+    S->stacksize=STACK_INIT_SIZE;
+    return OK;
+}
+int push(Sqstack *S,SElemType e)
+{
+    if(S->top-S->base>=S->stacksize)
+    {
+        S->base=(SElemType*)realloc(S->base, (S->stacksize+ STACKINCREMENT )*sizeof(SElemType));
+        if(!S->base)
+        return ERROR;
+    S->top=S->base+S->stacksize;
+    S->stacksize+=STACKINCREMENT;
+    }
+    *(S->top++) =e; 
+    return OK; 
+}
+int pop(Sqstack *S,SElemType *e)
+{
+    if(S->top==S->base)
+    {
+        return ERROR;
+    }
+    *e=*--(S->top);
+    return OK;
+}
+int GetTop(Sqstack *S,SElemType *e)
+{
+    if(S->top==S->base)
+    return ERROR;
+    *e=*(S->top-1);
+    return OK;
+}
