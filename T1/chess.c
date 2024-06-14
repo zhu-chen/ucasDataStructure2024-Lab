cb053bb8b4d93192fcc7dbb7dfb96b39eb8496f6
zhu-chen <1040555811@qq.com>
2024-06-14 15:22:05 +0800

    style:change the location of function declaration  to chess.c

--- T1/chess.c
+++ T1/chess.c
@@ -2,6 +2,45 @@
 
 int chessboard[8][8];
 
+int InitStack(Sqstack *S)
+{
+    S->base=(SElemType*)malloc(STACK_INIT_SIZE*sizeof(SElemType));
+    if(!S->base) 
+    return ERROR;
+    S->top=S->base;
+    S->stacksize=STACK_INIT_SIZE;
+    return OK;
+}
+int push(Sqstack *S,SElemType e)
+{
+    if(S->top-S->base>=S->stacksize)
+    {
+        S->base=(SElemType*)realloc(S->base, (S->stacksize+ STACKINCREMENT )*sizeof(SElemType));
+        if(!S->base)
+        return ERROR;
+    S->top=S->base+S->stacksize;
+    S->stacksize+=STACKINCREMENT;
+    }
+    *(S->top++) =e; 
+    return OK; 
+}
+int pop(Sqstack *S,SElemType *e)
+{
+    if(S->top==S->base)
+    {
+        return ERROR;
+    }
+    *e=*--(S->top);
+    return OK;
+}
+int GetTop(Sqstack *S,SElemType *e)
+{
+    if(S->top==S->base)
+    return ERROR;
+    *e=*(S->top-1);
+    return OK;
+}
+
 int main(){
     //TODO: ADD YOUR CODE HERE
     int x,y;
